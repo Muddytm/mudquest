c53187f7fac2910f54f1c30f68d0d808c4963140
calebh93 <hawkins.caleb93@gmail.com>
2016-04-13 13:27:49 -0700

    Up-to-date version

--- mudrpg/main.py
+++ mudrpg/main.py
@@ -8,18 +8,27 @@ async def run(client, message):
 
     data = json.load(open(data_loc, "r"))
 
+    # !mudrpg start
     if msg == "start" and not data["running"]:
         await client.send_message(message.channel, welcome())
 
         data["running"] = True
         with open(data_loc, "w+") as file:
             json.dump(data, file)
-    elif msg == "end" and data["running"]:
-        await client.send_message(message.channel, goodbye())
 
-        data["running"] = False
-        with open(data_loc, "w+") as file:
-            json.dump(data, file)
+    if data["running"]:
+        # !mudrpg end
+        elif msg == "end":
+            await client.send_message(message.channel, goodbye())
+
+            data["running"] = False
+            with open(data_loc, "w+") as file:
+                json.dump(data, file)
+        if data["gamestate"] == "new_or_load":
+            # !mudrpg new
+            elif msg == "new" and data["gamestate"] == "new_or_load":
+                #TODO: put stuff here
+                pass
 
 
 def welcome():
