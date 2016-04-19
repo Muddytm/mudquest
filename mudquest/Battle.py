341c9ca20683f7ee96e6e9ed446002d293962e70
calebh93 <hawkins.caleb93@gmail.com>
2016-04-19 15:03:19 -0700

    Note to self: await stuff.

--- mudquest/Battle.py
+++ mudquest/Battle.py
@@ -39,7 +39,7 @@ class Battle:
         if Game.hero == {}:
             self.make_hero(Simpleton(), Game)
 
-        self.cycle(Game, client, message)
+        await self.cycle(Game, client, message)
 
     async def cycle(self, Game, client, message):
         """Battle "cycle" that happens every full turn."""
